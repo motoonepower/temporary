b9082ffae7f764187d2a97369f0a9ef5947fbe27
Kneba <abenkenary3@gmail.com>
2022-01-21 10:04:15 +0700

    fixed error

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch potato_X00TD-userdebug
+export BUILD_USERNAME=tomcat
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 brunch X00TD
