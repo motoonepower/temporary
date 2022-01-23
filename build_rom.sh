c7e4168bf35a57cc6c55a8efe9ec03c9deca4b87
Kneba <abenkenary3@gmail.com>
2022-01-23 12:58:44 +0700

    rosie-lts

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/amyROM/manifest.git -b rosie -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/amyROM/manifest.git -b rosie-lts -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kneba/local_manifests.git --depth 1 -b amy .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch amy_X00TD-userdebug
+lunch amy_X00TD-eng
 export BUILD_USERNAME=venom
-export BUILD_HOSTNAME=matanza
-export TZ=Asia/Tokyo #put before last build command
-mka bacon
+export TZ=Asia/Jakarta #put before last build command
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
