83b423c4f8055f5c1a13503cf70ff46acfb56f4e
Asif <49162687+nhAsif@users.noreply.github.com>
2022-01-27 00:04:34 +0600

    rosy: fix sepolicy

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_rosy-userdebug
 export TZ=Asia/Dhaka 
+export TARGET_FLOS=true
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
