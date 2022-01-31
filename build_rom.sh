2540467c051ed52b2d274d28a853afefb8a678f7
Francesco Duca <60934657+FraSharp@users.noreply.github.com>
2022-01-31 12:43:45 +0100

    kebab: yaap-12: fix vintf manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/kebab-brickers/manifest_oneplus_kebab.git --depth 1 -b twelve .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
