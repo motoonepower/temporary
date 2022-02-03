dd95da8d2a9f906fbd841b3ea05a295d0ff4b22a
Ashish <97603141+JaswalAshish@users.noreply.github.com>
2022-02-03 09:33:15 +0530

    olivewood new update for project Elixir

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest.git -b snow  -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JaswalAshish/local_manifest.git --depth 1 -b pe-12 .repo/local_manifests
+git clone https://github.com/JaswalAshish/local_manifest.git --depth 1 -b ashish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
