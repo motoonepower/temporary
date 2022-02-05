640b578b49b301d2bf06deb76d24d3411a4c3769
Francesco Duca <60934657+FraSharp@users.noreply.github.com>
2022-02-05 21:44:13 +0100

    kebab: yaap-12: switch to userdebug build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch yaap_kebab-user
+lunch yaap_kebab-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export SKIP_ABI_CHECKS=true
 make yaap
