4d69ffae19e1dfd8b4bf9d213f0118686ffefd07
rahul <rahul-k@bigdi.cc>
2022-02-05 09:34:30 +0000

    vince: Arcana-testing build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rk134/local_manifests.git --depth 1 -b arcana .repo/local_manifests
+git clone https://github.com/rk134/local_manifests.git --depth 1 -b arcana-testing .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch aosp_vince-userdebug
-export KBUILD_BUILD_USER="rk134"
-export KBUILD_BUILD_HOST="CI-Lab"
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
