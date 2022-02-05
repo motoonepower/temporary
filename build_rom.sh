b6bfe3fa4511b23c6331c94ff87c853973089d9a
ucoboy <68426327+ucoboy@users.noreply.github.com>
2022-02-05 14:11:15 +0530

    Updated source

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Unknownbitch07/local_manifest.git --depth=1 -b xdroid .repo/local_manifests
+git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
