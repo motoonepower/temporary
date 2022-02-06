06b73f40ac5c730ae01ccf5143f75c8975dd6488
MOCHA <80810363+official-mocha@users.noreply.github.com>
2022-02-06 23:39:10 +0530

    Arcana a12

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/ma
 git clone https://github.com/official-mocha/local_manifest.git --depth 1 -b aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch aosp_tulip-userdebug
