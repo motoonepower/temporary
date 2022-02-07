4579f1aa0ea796e7b8db2137942fe8499b4fce65
D34DPUL <77775456+D34DPUL@users.noreply.github.com>
2022-02-07 09:04:15 +0530

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,8 @@ repo init --depth=1 --no-repo-verify -u git://github.com/StatiXOS/android_manife
 git clone https://github.com/D34DPUL/local_manifest_statix.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+
+# build rom 
 source build/envsetup.sh
 lunch statix_liber-userdebug
 export TZ=Asia/Dhaka #put before last build command
