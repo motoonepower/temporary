2b26c1a1fe21e59d8f461508dd96feb5754a8d0b
D34DPUL <77775456+D34DPUL@users.noreply.github.com>
2022-02-07 17:30:39 +0530

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/StatiXOS/android_manifest.git -b sc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/D34DPUL/local_manifest_statix.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/D34DPUL/local_manifest_statix.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
 lunch statix_liber-userdebug
 export TZ=Asia/Dhaka #put before last build command
-brunch liber
+brunch statix_liber-userdebug
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
