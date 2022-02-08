d8f6011061ec1f21e03e4f6a915cf8173cc01200
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-02-09 06:15:25 +0700

    regenerate

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build roms
 source build/envsetup.sh
 lunch xtended_X00TD-userdebug
-export SKIP_API_CHECKS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Hanoi #put before last build
 make xtended
 
