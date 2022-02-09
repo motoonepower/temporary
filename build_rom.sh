4c8e4742bccfb83c52bc463993da1cf42b4e2646
Kneba <abenkenary3@gmail.com>
2022-02-09 10:07:48 +0700

    vanilla build varients

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ export BUILD_USERNAME=tomcat
 export SKIP_API_CHECKS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Bangkok #put before last build command
-blissify -g X00TD
+blissify -v X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
