eda808f5c7ba48006d960bf4cae29cf6e2fc6aa6
D34DPUL <77775456+D34DPUL@users.noreply.github.com>
2022-02-09 20:43:09 +0530

    Initial A12 build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b r -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b arcadia -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/D34DPUL/bliss_local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -10,10 +10,4 @@ export TZ=Asia/Kolkata #put before last build command
 blissify -g liber
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-#rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P 
-rclone copy out/target/product/liber/boot.img cirrus:liber/bliss_29-9 -P 
-rclone copy out/target/product/liber/dtbo.img cirrus:liber/bliss_29-9 -P 
-rclone copy out/target/product/liber/system.img cirrus:liber/bliss_29-9 -P 
-rclone copy out/target/product/liber/product.img cirrus:liber/bliss_29-9 -P 
-rclone copy out/target/product/liber/vbmeta.img cirrus:liber/bliss_29-9 -P 
-rclone copy out/target/product/liber/vendor.img cirrus:liber/bliss_29-9 -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P 
