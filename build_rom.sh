92e78c55e4eb322814363fc730851c353d20c50e
rahul <rahul-k@bigdi.cc>
2022-02-13 07:52:25 +0000

    vince: initialize dream release

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom 
+# sync rom  
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rk134/local_manifests.git --depth 1 -b arcana-testing .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
