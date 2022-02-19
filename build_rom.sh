6a113e146cc89e20aeba6f55ad60cf5089db01c3
sajidshahriar <sazidshahriar39@gmail.com>
2022-02-19 14:53:52 +0600

    Bery - POS

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelOS-Pixelish/manifest -b staging/twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b pos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
