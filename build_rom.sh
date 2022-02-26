c87b1dc3656f4543102a74d2d7761f875ea08170
MukulSharma06175 <sharmamukul06175@gmail.com>
2022-02-26 15:25:48 +0530

    FIx sync

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/lighthouse-os/manifest.
 git clone https://github.com/MukulSharma06175/Local-Manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
                                               
-# build rom
+# build  rom
 source build/envsetup.sh
 lunch lighthouse_lavender-user
 export TZ=Asia/Kolkata #put before last build command
