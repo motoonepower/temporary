968b8a06d1732b1946020c2e6814539168345f46
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-03-09 18:36:08 +0700

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
-# sync roms
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-mips,-darwin,-notdefault
+# sync rom
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b sakura .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=tiktod
-export BUILD_HOSTNAME=feat-gusion
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Bangkok #put before last build command
 mka bacon
 
