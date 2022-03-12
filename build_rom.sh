fb7ec27eb7cbc6a89badad9d3a3f3f825952692b
lazr <93397127+lazrdev@users.noreply.github.com>
2022-03-12 12:12:16 +0100

    initial gapps user build without gcam go

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest.git -b frico-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lazrdev/local_manifest.git --depth 1 -b potato-12-mi439 .repo/local_manifests
+git clone https://github.com/lazrdev/local_manifest.git --depth 1 -b potato-12-mi439-gapps .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch potato_mi439-userdebug
+lunch potato_mi439-user
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
 brunch mi439
 
