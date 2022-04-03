3d28a50385d47e94bd564ed569c64792500f4491
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-04-03 12:00:40 +0600

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b blaze .repo/local_manifests
+git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 source build/envsetup.sh
-lunch blaze_X00TD-userdebug
+lunch blaze_alioth-userdebug
 export WITH_GAPPS=true
-export BUILD_USERNAME=siren
 export TZ=Asia/Tokyo #put before last build command
-brunch X00TD
+brunch alioth
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
