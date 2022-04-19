8efa2fdf3807eb0150e72ab0931e89a9da248560
Hakan <hknhzl@gmail.com>
2022-04-19 12:53:18 +0300

    Fix typo

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Lafactorial/local_manifest.git --depth 1 -b AEX .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command
-export WITH_GAPPS := true
 m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
